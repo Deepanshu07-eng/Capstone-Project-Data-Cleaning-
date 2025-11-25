7e4aea2b5265e907b270bc52777519339c12d247
Deepanshu <d2004gautam@gmail.com>
2025-11-25 22:28:31 +0530

    Removing duplicates, replacing negative salary and invalid experience from dataset

--- Data_Clean.py
+++ Data_Clean.py
@@ -23,9 +23,8 @@ df['Education'] = df['Education'].fillna(df['Education'].mode()[0])
 df['Industry'] = df['Industry'].fillna(df['Industry'].mode()[0])
 df['Location'] = df['Location'].fillna(df['Location'].mode()[0])
 
-df['AI Risk'] = df['AI Risk'].astype(str).str.lower().str.strip()
-df['AI Risk'] = df['AI Risk'].replace('nan', pd.NA)
-df['AI Risk'] = df['AI Risk'].fillna(df['AI Risk'].mode()[0])
+df['AI Risk'] = df['AI Risk'].astype(str).str.lower().str.strip().replace('nan', pd.NA).fillna(df['AI Risk'].mode()[0])
+
 
 df['Years of Experience'] = df['Years of Experience'].fillna(df['Years of Experience'].median())
 df['Monthly Salary (INR)'] = df['Monthly Salary (INR)'].fillna(df['Monthly Salary (INR)'].median())
@@ -33,3 +32,18 @@ df['Monthly Salary (INR)'] = df['Monthly Salary (INR)'].fillna(df['Monthly Salar
 
 print("\nMissing values after cleaning:")
 print(df.isnull().sum())
+
+# Removing duplicates
+
+print("Duplicates before cleaning:", df.duplicated().sum())   # in this data set, There is no duplicate values
+
+#Replace negative salaries with NaN and then fill them with median
+
+df['Monthly Salary (INR)'] = np.where(df['Monthly Salary (INR)'] < 0 , df['Monthly Salary (INR)'].mean(), df['Monthly Salary (INR)'])
+print("\nNegative salaries replaced. Any negative values now:", (df['Monthly Salary (INR)'] < 0).sum())   # there is no negative values in this data set
+
+# replace invalid experience values with median
+df['Years of Experience'] = np.where(df['Years of Experience'] < 0 , df['Years of Experience'].median(), df['Years of Experience'])
+print("\nNegative experience values replaced. Any negative values now:", (df['Years of Experience'] < 0).sum())   # there is no negative values in this data set
+
+
